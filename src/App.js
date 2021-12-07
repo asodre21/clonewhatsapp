33129da87d91b5539dc37d222883e38fab69a134
asodre21 <skynetwebsystem@gmail.com>
2021-12-07 15:38:06 -0300

    new commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
 import './App.css';
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+
+       <header>
+         <img className="header--avatar" src="https://i0.wp.com/maikon.biz/wp-content/uploads/2020/06/gerador-de-persona-maikonbiz.png?fit=1080%2C1080&ssl=1" alt="" />
+         <div className="header--buttons">
+          ...
+         </div>
+       </header>
+
+       <div className="search">
+         ...
+       </div>
+
+       <div className="chatlist">
+         ...
+       </div>
+
+       </div>
+       <div className="contentarea">
+        ...
+      </div>
+
     </div>
   );
 }
-
-export default App;
