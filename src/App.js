f4aa3b841648fda849e90fcfb49610d23156a6eb
asodre21 <skynetwebsystem@gmail.com>
2021-12-07 16:17:38 -0300

    new css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import './App.css';
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
 export default () => {
   return (
     <div className="app-window">
@@ -10,12 +15,23 @@ export default () => {
        <header>
          <img className="header--avatar" src="https://i0.wp.com/maikon.biz/wp-content/uploads/2020/06/gerador-de-persona-maikonbiz.png?fit=1080%2C1080&ssl=1" alt="" />
          <div className="header--buttons">
-          ...
+           <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}} />
+           </div>
+           <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}} />
+           </div>
+           <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}} />
+           </div>
          </div>
        </header>
 
        <div className="search">
-         ...
+         <div className="search--input">
+             <SearchIcon style={{color: '#919191'}} />
+             <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+         </div>
        </div>
 
        <div className="chatlist">
